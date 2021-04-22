75970ba2619b901c20be825294c7228640d1f3c1
Thomg102 <69638420+Thomg102@users.noreply.github.com>
2021-04-22 23:17:28 +0700

    Create Database

---
+++ src/config/db/index.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+async function connect() {
+
+    try {
+        await mongoose.connect('mongodb://localhost:27017/WebTheThao', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+        console.log('success!!');
+    } catch (error) {
+        console.log('fail!!');
+    }
+}
+
+module.exports = { connect };
--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const route = require('./routes/');
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({
-  extended: true
+    extended: true
 }));
 app.use(express.json());
 //HTTP logger
@@ -24,16 +24,20 @@ app.use(methodOverride('_method'));
 //Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'resources','views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 route(app);
 
-var hello = () => "Hello" 
+//connect db
+const db = require('./config/db/index') //chứa hàm connect()
+db.connect();
 
-function HelloWorld(name)
-{
-  return name}
+var hello = () => "Hello"
+
+function HelloWorld(name) {
+    return name
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
